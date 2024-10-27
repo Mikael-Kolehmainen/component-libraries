7c8914f5e1cef8c3ad2d3b7909155df342fef511
Mikael Kolehmainen <mikaelkolehmainen05@gmail.com>
2024-10-27 14:45:27 +0200

    Added link variant of icons

--- react/src/components/icon/icon.component.tsx
+++ react/src/components/icon/icon.component.tsx
@@ -1,16 +1,22 @@
-import { IconContainer } from "./icon.styles";
+import { IconContainer, IconLinkContainer } from "./icon.styles";
 
 interface IconProps {
-  IconComponent: React.FC
-  onClick?: () => void
-  width?: string
-  height?: string
-  color?: string
-  style?: React.CSSProperties
+  IconComponent: React.FC;
+  link?: string;
+  target?: string;
+  onClick?: () => void;
+  width?: string;
+  height?: string;
+  color?: string;
+  style?: React.CSSProperties;
 }
 
-const Icon = ({ IconComponent, onClick, width='70px', height='70px', color, style }: IconProps) => {
-  return (
+const Icon = ({ IconComponent, link, target, onClick, width = "70px", height = "70px", color, style }: IconProps) => {
+  return link ? (
+    <IconLinkContainer href={link} target={target ? target : "_self"} $width={width} $height={height} $color={color}>
+      <IconComponent style={style} />
+    </IconLinkContainer>
+  ) : (
     <IconContainer onClick={onClick} $width={width} $height={height} $color={color}>
       <IconComponent style={style} />
     </IconContainer>
--- react/src/components/icon/icon.styles.tsx
+++ react/src/components/icon/icon.styles.tsx
@@ -11,6 +11,23 @@ export const IconContainer = styled.div<IconContainerProps>`
     display: block;
     width: ${(props) => props.$width};
     height: ${(props) => props.$height};
-    color: ${(props) => props.$color ?? props.theme.primaryColor};
+    color: ${(props) => props.$color ?? props.theme.iconColor};
+  }
+`;
+
+export const IconLinkContainer = styled.a<IconContainerProps>`
+  svg {
+    display: block;
+    width: ${(props) => props.$width};
+    height: ${(props) => props.$height};
+    color: ${(props) => props.$color ?? props.theme.iconColor};
+  }
+
+  &:hover {
+    cursor: pointer;
+
+    svg {
+      color: ${(props) => props.theme.iconHoverColor};
+    }
   }
 `;
